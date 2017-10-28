39604dbee3f7100eadb0f3fa39e1ce879870e7ba
heiniumiao <baiyanhong0321@gmail.com>
2017-10-29 02:56:26 +0300

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only:[:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only:[:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups =Group.all
@@ -44,6 +44,31 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group =Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "Joined Succeed!"
+      else
+        flash[:warning] = "You are already Group Member!"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert]="Quitted!"
+      else
+        flash[:warning]="You are not Group Member, can't quit..."
+      end
+
+      redirect_to group_path(@group)
+    end
 
   private
 
